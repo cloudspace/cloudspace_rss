41229be519e9345f54d82be9b152c9c818ab5efc
Nicholas Thomson <nick@cloudspace.com>
2013-04-10 16:22:28 -0400

    Added unicorn gem, added deploy tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,3 +11,35 @@ set :repository, "git@github.com:cloudspace/cloudspace_rss.git"
 set :deploy_to, "/srv/www/#{application}"
 set :deploy_via, :remote_cache
 set :rails_env, 'production'
+
+namespace :deploy do
+  task :start do
+    run "cd #{current_release} && bundle exec unicorn -E #{rails_env} -c /etc/unicorn/wetfeet.com.rb -D"
+  end
+
+  task :stop do
+    run "kill -QUIT $(cat /var/run/wetfeet.com_unicorn.pid)"
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "kill -USR2 $(cat /var/run/wetfeet.com_unicorn.pid)"
+  end
+
+  namespace :db do
+    task :drop do
+      run "cd #{current_release} && RAILS_ENV=#{rails_env} bundle exec rake db:drop"
+    end
+    
+    task :create do
+      run "cd #{current_release} && RAILS_ENV=#{rails_env} bundle exec rake db:create"
+    end
+    
+    task :migrate do
+      run "cd #{current_release} && RAILS_ENV=#{rails_env} bundle exec rake db:migrate"
+    end
+    
+    task :seed do
+      run "cd #{current_release} && RAILS_ENV=#{rails_env} bundle exec rake db:seed"
+    end
+  end
+end
