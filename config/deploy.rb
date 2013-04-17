d7759f506058c60f96f7f963c4852d8ac2d50c0f
Nicholas Thomson <nick@cloudspace.com>
2013-04-17 11:15:44 -0400

    Add bundler tasks to deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,3 +47,39 @@ namespace :deploy do
     end
   end
 end
+
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(release_path, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+
+  task :install, :roles => :app do
+    run "cd #{release_path} && bundle install --binstubs "
+
+    on_rollback do
+      system "say \" something messed up rolling back\""
+      if previous_release
+        run "cd #{previous_release} && sudo bundle install --binstubs "
+      else
+        logger.important "no previous release to rollback to, rollback of bundler:install skipped"
+      end
+    end
+  end
+
+  task :bundle_new_release, :roles => :db do
+    bundler.create_symlink
+    bundler.install
+  end
+end
+
+
+after "deploy:rollback:revision", "bundler:install"
+after "deploy:update_code", "bundler:bundle_new_release"
+after "deploy:update_code", "deploy:db:migrate"
+after "deploy:update_code", "deploy:db:seed"
+after "deploy", "deploy:cleanup"
+# after "deploy:stop",    "delayed_job:stop"
+# after "deploy:start",   "delayed_job:start"
