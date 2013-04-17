d7d0a383497e10b11ad9ce9f0ffa52a767ef1f37
Matthew Cupples <nowthatsamatt@gmail.com>
2013-04-17 10:59:06 -0400

    stupid joey

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,20 @@
 set :stages, %w(production staging)
 set :default_stage, "staging"
+
+require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
 
 set :application, "rss.cloudspace.com"
 set :user, "root"
 set :group, "root"
 set :ssh_options, { :forward_agent => true }
+default_run_options[:pty] = true
 set :scm, :git
 set :repository, "git@github.com:cloudspace/cloudspace_rss.git"
 set :deploy_to, "/srv/www/#{application}"
 set :deploy_via, :remote_cache
 set :rails_env, 'production'
+set :bundle_flags, "--deployment --quiet --binstubs=sbin"
 
 namespace :deploy do
   task :start do
