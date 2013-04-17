7bccf3b0da01ce2088dee3962c6d719c09da6743
Matthew Cupples <nowthatsamatt@gmail.com>
2013-04-17 11:34:04 -0400

    joey is WRONG

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,16 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Emanuel', city: cities.first)
 feeds = Feed.create([
-          {name: 'Engadget',        url: 'http://www.engadget.com/rss.xml'},
-          {name: 'The Verge',       url: 'http://www.theverge.com/rss/index.xml'},
-          {name: 'CNN Top Stories', url: 'http://rss.cnn.com/rss/cnn_topstories.rss'},
-          {name: 'CNN World',       url: 'http://rss.cnn.com/rss/cnn_world.rss'},
-          {name: 'CNN U.S.',        url: 'http://rss.cnn.com/rss/cnn_us.rss'},
-          {name: 'CNN Business',    url: 'http://rss.cnn.com/rss/money_latest.rss'},
-          {name: 'CNN Politics',    url: 'http://rss.cnn.com/rss/cnn_allpolitics.rss'},
-          {name: 'CNN Crime',       url: 'http://rss.cnn.com/rss/cnn_crime.rss'},
-          {name: 'CNN Technology',  url: 'http://rss.cnn.com/rss/cnn_tech.rss'},
+          {:name => 'Engadget',        :url => 'http://www.engadget.com/rss.xml'},
+          {:name => 'The Verge',       :url => 'http://www.theverge.com/rss/index.xml'},
+          {:name => 'CNN Top Stories', :url => 'http://rss.cnn.com/rss/cnn_topstories.rss'},
+          {:name => 'CNN World',       :url => 'http://rss.cnn.com/rss/cnn_world.rss'},
+          {:name => 'CNN U.S.',        :url => 'http://rss.cnn.com/rss/cnn_us.rss'},
+          {:name => 'CNN Business',    :url => 'http://rss.cnn.com/rss/money_latest.rss'},
+          {:name => 'CNN Politics',    :url => 'http://rss.cnn.com/rss/cnn_allpolitics.rss'},
+          {:name => 'CNN Crime',       :url => 'http://rss.cnn.com/rss/cnn_crime.rss'},
+          {:name => 'CNN Technology',  :url => 'http://rss.cnn.com/rss/cnn_tech.rss'},
         ])
