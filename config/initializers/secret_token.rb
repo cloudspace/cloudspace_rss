c435743d0dd4f9fde3a2baa8fc5202cfc70e9497
Adam Dunson <adam@cloudspace.com>
2014-02-28 16:20:13 -0500

    [Fix #66690320] Remove secret token, update README

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CloudspaceRss::Application.config.secret_token = '1e9753eead85eb0540e12bb3ead3dcad8bf90c4c31c1e6701d53d348cfc23f5ba4090bc4c90b497cf7145b7320c8ce969a30b6e16eefe1ef6ec0801bb1ea64d2'
+CloudspaceRss::Application.config.secret_token =  ENV['SECRET_TOKEN']
